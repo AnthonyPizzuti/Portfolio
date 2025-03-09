11ad57affda4fc892b0be3c5d0c86b92b29bee16
Anthony Pizzuti <anthony.pizzuti@gmx.de>
2025-03-10 00:01:19 +0100

    footer finish

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,8 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  currentLanguage: string = 'en';
 
+  switchLanguage(lang: string) {
+    this.currentLanguage = lang;
+    document.documentElement.lang = lang;
+    // Hier kannst du eine Übersetzungslogik einfügen
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
