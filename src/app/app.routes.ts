97c887e040f983458bb83d1f616bf4c79c8cb756
Anthony Pizzuti <anthony.pizzuti@gmx.de>
2025-03-17 23:14:16 +0100

    about me finish, skill set finish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ContactComponent } from './contact/contact.component';
+import { CommonModule } from '@angular/common';
 
 export const routes: Routes = [
   { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled' })],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes, { anchorScrolling: 'enabled' }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent {
+  animationState: 'closed' | 'opening' | 'open' | 'closing' = 'closed';
 
+  toggleSticker() {
+    if (this.animationState === 'closed' || this.animationState === 'closing') {
+      this.animationState = 'opening';
+      setTimeout(() => (this.animationState = 'open'), 800);
+    } else if (
+      this.animationState === 'open' ||
+      this.animationState === 'opening'
+    ) {
+      this.animationState = 'closing';
+      setTimeout(() => (this.animationState = 'closed'), 800);
+    }
+  }
 }
